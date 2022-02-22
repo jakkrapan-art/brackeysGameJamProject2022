334889ff4bc2737421dea45c42e4ee7f7bfc5635
Art <jakkrapan.art@mail.kmutt.ac.th>
2022-02-22 20:19:14 +0700

    Add Bedroom Bookshelf(IteractObject)

---
+++ Assets/Scripts/Objects/InteracableObjects/Bed_Bookshelf.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bed_Bookshelf : InteracableObject
+{
+    public override void Interact()
+    {
+        base.Interact();
+    }
+
+    protected override void ObjectAction()
+    {
+        base.ObjectAction();
+    }
+}
