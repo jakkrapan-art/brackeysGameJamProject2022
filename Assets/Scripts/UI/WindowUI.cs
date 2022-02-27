5f3357106bd09bb70b0d0deaf93e40770768717a
Pete <thannakorn.lectival@mail.kmutt.ac.th>
2022-02-27 12:30:01 +0700

    Update WindowUI script

--- Assets/Scripts/UI/WindowUI.cs
+++ Assets/Scripts/UI/WindowUI.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class WindowUI : MonoBehaviour
 {
- 
+    public GameObject myobject;
+    public bool isActivate;
+    public KeyCode keyBlind;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyBlind) && isActivate == false)
+        {
+            myobject.SetActive(true);
+            isActivate = true;
+        }
+        else if (Input.GetKeyDown(keyBlind) && isActivate == true)
+        {
+            myobject.SetActive(false);
+            isActivate = false;
+        }
+    }
+
+
 }
